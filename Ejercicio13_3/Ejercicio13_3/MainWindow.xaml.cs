214487a7309424b40b9f3eef900bd13fe3d3b9dc
Paula Atencia Barranco <paulaatencia56@gmail.com>
2024-12-14 13:50:57 +0100

    Ejercicio3

---
+++ Ejercicio13_3/Ejercicio13_3/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Ejercicio13_3
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ColorComboBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Configurar ToolTip inicial
+            if (ColorComboBox.SelectedItem is ComboBoxItem selectedItem)
+            {
+                ToolTipService.SetToolTip(ColorComboBox, selectedItem.Tag);
+            }
+        }
+
+        private void ColorComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ColorComboBox.SelectedItem is ComboBoxItem selectedItem)
+            {
+                ToolTipService.SetToolTip(ColorComboBox, selectedItem.Tag);
+            }
+        }
+    }
+}
